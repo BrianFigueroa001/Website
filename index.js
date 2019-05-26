7541c569b7e2058d827c79d3daaf404eb9f29e9e
Brian Figueroa <brianfigueroa001@gmail.com>
2019-05-26 16:42:41 -0400

    Small changes to html/css and added jQuery for user interaction

--- index.js
+++ index.js
@@ -1,26 +1,95 @@
 $(document).ready(function() {
-    //All custom JQuery methods go here
-    // $("div").hide(); //Clear screen
 
-    //Elements fade in
-    // $("#infoSections").delay(500).fadeIn("slow");
+/*The following code adds user interactibility for when they hover over the tabs*/
 
-    // $("#educationSection").delay(0).fadeIn(0, function(){
-    //     $("#educationSection").slideUp(0);
-    // });
+    //When user hovers over the home tab
+    $("#homeClickable").hover( function() { //Cursor enters
+        //Highlight home tab
+        $(this).css("background-color", "#222cb7"); 
 
-    // $("#experienceSection").delay(0).fadeIn(0, function(){
-    // 	$("#experienceSection").slideUp(0);
-    // });
+        //User clicks home tab
+        $(this).click( function () {
+            //If the home section is hidden
+            if ($("#homeSection").css("visibility") === "hidden")
+            {
+                //Leave only home section visible
+                $("#homeSection").css("visibility", "visible");
+                $("#educationSection").css("visibility", "hidden");
+                $("#experienceSection").css("visibility", "hidden");
+                $("#linksSection").css("visibility", "hidden");
+            }
+        });
 
-    // $("#linksSection").delay(0).fadeIn(0, function(){
-    // 	$("#linksSection").slideUp(0);
-    // });
+    } , function() { //Cursor leaves
+        $(this).css("background-color", "#373A67"); //Remove highlight
+    });
 
-    // $("#educationClickable").delay(800).fadeIn("slow");
+    //When user hovers over the education tab
+    $("#educationClickable").hover( function() {
+        //Highlight education tab
+        $(this).css("background-color", "#222cb7");
 
-    // $("#experienceClickable").delay(800).fadeIn("slow");
+        //User clicks education tab
+        $(this).click( function () {
+            //If the education section is hidden
+            if ($("#educationSection").css("visibility") === "hidden")
+            {
+                //Leave only education section visible
+                $("#homeSection").css("visibility", "hidden");
+                $("#educationSection").css("visibility", "visible");
+                $("#experienceSection").css("visibility", "hidden");
+                $("#linksSection").css("visibility", "hidden");
+            }
+        });
 
-    // $("#linksClickable").delay(800).fadeIn("slow");
+    } , function() {
+        $(this).css("background-color", "#373A67"); //Remove highlight
+    });
+
+    //When user hovers over the experience tab
+    $("#experienceClickable").hover( function() {
+        //Highlight experience tab
+        $(this).css("background-color", "#222cb7"); 
+
+        //User clicks experience tab
+        $(this).click( function() {
+            //If the experience section is hidden
+            if ($("#experienceSection").css("visibility") === "hidden")
+            {
+                //Leave only experience section visible
+                $("#homeSection").css("visibility", "hidden");
+                $("#educationSection").css("visibility", "hidden");
+                $("#experienceSection").css("visibility", "visible");
+                $("#linksSection").css("visibility", "hidden");
+            }
+        });
+
+    } , function() {
+        $(this).css("background-color", "#373A67"); //Remove highlight
+    });
+
+    //When user hovers over the links tab
+    $("#linksClickable").hover( function() {
+        //Highlight link tab
+        $(this).css("background-color", "#222cb7");
+
+        //User clicks links tab
+        $(this).click( function() {
+            //If the link section is hidden
+            if ($("#linksSection").css("visibility") === "hidden")
+            {
+                //Leave only link section visible
+                $("#homeSection").css("visibility", "hidden");
+                $("#educationSection").css("visibility", "hidden");
+                $("#experienceSection").css("visibility", "hidden");
+                $("#linksSection").css("visibility", "visible");
+            }
+        });
+
+    } , function() {
+        $(this).css("background-color", "#373A67"); //Remove highlight
+    });
+
+
 
 });
