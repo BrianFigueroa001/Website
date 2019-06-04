fa6ae12ace2ded62c499930ef426a76715e73952
Brian Figueroa <brianfigueroa001@gmail.com>
2019-06-04 04:33:20 -0400

    Optimized code in JS file

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ $(document).ready(function() {
     $("#educationClickable").fadeOut(0);
     $("#experienceClickable").fadeOut(0);
     $("#linksClickable").fadeOut(0);
+
     //Automatically light up home tab since home section is initially displayed.
     $("#homeClickable").css("text-shadow", "2px 2px 8px white");
-
-    //Gets rid of that split second text overlapping home section upon opening web-page
+    //Gets rid of overlapping sections for the split-second duration upon opening the website
     $("#infoSections").css("visibility", "visible");
 
-/*The initial elements to be displayed (background, home section, and all tabs)*/
+    /*The initial elements to be displayed (background, home section, and all tabs)*/
     $("#infoSections").fadeIn(500);
     $("#homeSection").fadeIn(600);
     $("#clickableTabs").fadeIn(700);
@@ -29,115 +29,89 @@ $(document).ready(function() {
 
 
 
-/*The following code allow users to switch information sections.*/
+/*The following code allow users to switch information sections by selecting tabs.*/
 
     //When user hovers over the home tab
-    $("#homeClickable").hover( function() { //Cursor enters
-        //Highlight home tab
-        $(this).css("background-color", "#21244c"); 
-
-        //User clicks home tab
-        $(this).click( function () {
-            $("#educationSection").fadeOut(0);
-            $("#educationClickable").css("text-shadow", "none");
-
-            $("#experienceSection").fadeOut(0);
-            $("#experienceClickable").css("text-shadow", "none");
-
-            $("#linksSection").fadeOut(0);
-            $("#linksClickable").css("text-shadow", "none");
-
-            $("#homeSection").fadeIn(500);
-            $(this).css("text-shadow", "2px 2px 8px white"); //Light up the home tab's text
-
-            $("body").css("background-image", "url('homeTest.jpg')");
-
-        });
-
-    } , function() { //Cursor leaves
+    $("#homeClickable").hover( function() {
+        $(this).css("background-color", "#21244c"); //Highlight home tab
+    } , function() { 
         $(this).css("background-color", "#373A67"); //Remove highlight over where the cursor is pointing
     });
 
     //When user hovers over the education tab
     $("#educationClickable").hover( function() {
-        //Highlight education tab
-        $(this).css("background-color", "#21244c"); 
-
-        //User clicks education tab
-        $(this).click( function () {
-            //Display education section and hide all the others
-            $("#homeSection").fadeOut(0);
-            $("#homeClickable").css("text-shadow", "none");
-
-            $("#experienceSection").fadeOut(0);
-            $("#experienceClickable").css("text-shadow", "none");
-
-            $("#linksSection").fadeOut(0);
-            $("#linksClickable").css("text-shadow", "none");
-
-            $("#educationSection").fadeIn(500);
-            $(this).css("text-shadow", "2px 2px 8px white"); //Light up education tab's text
-
-            $("body").css("background-image", "url('educationTest.jpg')");
-        });
-
+        $(this).css("background-color", "#21244c"); //Highlight education tab
     } , function() {
         $(this).css("background-color", "#373A67"); //Remove highlight over where the cursor is pointing
     });
 
     //When user hovers over the experience tab
     $("#experienceClickable").hover( function() {
-        //Highlight experience tab
-        $(this).css("background-color", "#21244c");  
-
-        //User clicks experience tab
-        $(this).click( function() {
-            //Display the experience section and hide all the others.
-            $("#homeSection").fadeOut(0);
-            $("#homeClickable").css("text-shadow", "none"); 
-
-            $("#educationSection").fadeOut(0);
-            $("#educationClickable").css("text-shadow", "none");
-
-            $("#linksSection").fadeOut(0);
-            $("#linksClickable").css("text-shadow", "none");
-
-            $("#experienceSection").fadeIn(500);
-            $(this).css("text-shadow", "2px 2px 8px white"); //Light up the experience tab's text
-
-            $("body").css("background-image", "url('expTest.jpg')");
-
-        });
-
+        $(this).css("background-color", "#21244c"); //Highlight experience tab
     } , function() {
         $(this).css("background-color", "#373A67"); //Remove highlight over where the cursor is pointing
     });
 
     //When user hovers over the links tab
     $("#linksClickable").hover( function() {
-        //Highlight link tab
-        $(this).css("background-color", "#21244c"); 
-
-        //User clicks links tab
-        $(this).click( function() {
-            //Display the experience section and hide all the others.
-            $("#homeSection").fadeOut(0);
-            $("#homeClickable").css("text-shadow", "none"); 
-
-            $("#educationSection").fadeOut(0);
-            $("#educationClickable").css("text-shadow", "none");
-
-            $("#experienceSection").fadeOut(0);
-            $("#experienceClickable").css("text-shadow", "none"); 
-
-            $("#linksSection").fadeIn(500);
-            $(this).css("text-shadow", "2px 2px 8px white"); //Light up the links tab's text
-            $("body").css("background-image", "url('linkTest.jpg')");
-
-        });
-
+        $(this).css("background-color", "#21244c"); //Highlight link tab
     } , function() {
         $(this).css("background-color", "#373A67"); //Remove highlight over where the cursor is pointing
     });
 
+    //When any of the tabs are clicked.
+    $("#homeClickable").click( function() {
+        clickTab(this, "#homeSection", "url('homeTest.jpg')");
+    });
+
+    $("#educationClickable").click( function() {
+        clickTab(this, "#educationSection", "url('educationTest.jpg')");
+    });
+
+    $("#experienceClickable").click( function() {
+        clickTab(this, "#experienceSection", "url('expTest.jpg')");
+    });
+
+    $("#linksClickable").click( function() {
+        clickTab(this, "#linksSection", "url('linkTest.jpg')");
+    });
+
 });
+
+/*
+Displays the section the user wants to see, and hides every other.
+Also lights up the tab that the user selected while de-lighting every other.
+Parameters: jQuery object, string containing section, string containing img url
+*/
+function clickTab(selectedTab, selectedSection, backgroundImage) {
+
+    //Fade out the non-selected sections and remove the lighting
+    if (!$("#homeClickable").is(selectedTab))
+    {
+        $("#homeSection").fadeOut(0);
+        $("#homeClickable").css("text-shadow", "none"); 
+    }
+
+    if (!$("#educationClickable").is(selectedTab))
+    {
+        $("#educationSection").fadeOut(0);
+        $("#educationClickable").css("text-shadow", "none"); 
+    }
+
+    if (!$("#experienceClickable").is(selectedTab))
+    {
+        $("#experienceSection").fadeOut(0);
+        $("#experienceClickable").css("text-shadow", "none"); 
+    }
+
+    if (!$("#linksClickable").is(selectedTab))
+    {
+        $("#linksSection").fadeOut(0);
+        $("#linksClickable").css("text-shadow", "none"); 
+    }
+
+        //Display the selected section and light up its corresponding tab.
+    $("body").css("background-image", backgroundImage);
+    $(selectedSection).fadeIn(500);
+    $(selectedTab).css("text-shadow", "2px 2px 8px white");
+}
