f7a8e90b7a84b2ec9a5966ce40f11fd0ced8f5b0
Brian Figueroa <brianfigueroa001@gmail.com>
2019-07-02 18:56:53 -0400

    Improved code readability by providing internal documentation

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
-    initializeDisplay();
 
-    //Stores data containing each section and their associated tab and background.
-    let homeDisplay = {section: "#homeSection", tab: "#homeTab", bgImage: "#bg1"}; 
-    let linkDisplay = {section: "#linkSection", tab: "#linkTab", bgImage: "#bg2"};
-    let eduDisplay = {section: "#eduSection", tab: "#eduTab", bgImage: "#bg3"};
-    let expDisplay = {section: "#expSection", tab: "#expTab", bgImage: "#bg4"};
+    initializeDisplay(); //Fade in elements and take user to the General Information (HOME) Section
+
+    //Stores each "display" made up of their three related elements (sections, tabs, and backgrounds).
+    let homeDisplay = {section: "#homeSection", tab: "#homeTab", bgImage: "#bg1"}; //General Information
+    let linkDisplay = {section: "#linkSection", tab: "#linkTab", bgImage: "#bg2"}; //Links and Projects
+    let eduDisplay = {section: "#eduSection", tab: "#eduTab", bgImage: "#bg3"}; //Education History
+    let expDisplay = {section: "#expSection", tab: "#expTab", bgImage: "#bg4"}; //Work Experience
 
     let currentDisplay = homeDisplay; //Tracks the current display
 
-    //When user hovers over a tab.
+    //When user hovers over a tab (buttons that say General Information, Links & Projects, Education History, and Work Experience).
     $("#homeTab, #linkTab, #eduTab, #expTab").hover(function() {
-        $(this).css("background-color", "#21244c"); //Highlight tab
+        $(this).css("background-color", "#21244c"); //Highlight clicked tab/button
     } , function() { 
         $(this).css("background-color", "#373A67"); //Remove highlight
     });
@@ -19,22 +20,22 @@ $(document).ready(function() {
     //When user clicks any of the tabs.
     $("#homeTab, #linkTab, #eduTab, #expTab").click(function() {
         if ($(this).is("#homeTab")) {
-            changeDisplay(homeDisplay, currentDisplay);
-            currentDisplay = homeDisplay;
+            changeDisplay(homeDisplay, currentDisplay); //Change display to the General Information/Home 
+            currentDisplay = homeDisplay; //Update the current display
         }
 
         else if ($(this).is("#linkTab")) {
-            changeDisplay(linkDisplay, currentDisplay);
+            changeDisplay(linkDisplay, currentDisplay); //Change display to the Links & Projects 
             currentDisplay = linkDisplay;
         }
 
         else if ($(this).is("#eduTab")) {
-            changeDisplay(eduDisplay, currentDisplay);
+            changeDisplay(eduDisplay, currentDisplay); //Change display to the Education History
             currentDisplay = eduDisplay;
         }
 
         else {
-            changeDisplay(expDisplay, currentDisplay);
+            changeDisplay(expDisplay, currentDisplay); //Change display to the work experience
             currentDisplay = expDisplay;
         }
     });
@@ -65,56 +66,54 @@ function changeDisplay(newDisplay, currentDisplay) {
     if (newDisplay !== currentDisplay) {
         updateTabLighting(newDisplay, currentDisplay);
         updateBackgroundIndex(newDisplay, currentDisplay);
-        // updateSectionIndex(newDisplay, currentDisplay);
         crossfade(newDisplay, currentDisplay);
     }
+}
 
-
+//Light up the tab that the user clicked on, and turn off the previous.
+function updateTabLighting(newDisplay, currentDisplay) {
+    $(newDisplay.tab).css("text-shadow", "2px 2px 8px white");
+    $(currentDisplay.tab).css("text-shadow", "none");
 }
 
 
 //Make sure that the new background has a lower z-index value than the current background.
 function updateBackgroundIndex(newDisplay, currentDisplay) {
-    //Note that .css({property}) seems to return a string. Must parse into an int for comparisons.
 
+    //Note that $(selector).css("property") function returns a string. Must parse into an int for comparisons.
     let newBGPos = parseInt($(newDisplay.bgImage).css("z-index")); //Get z-index position of new background as an integer
     let currentBGPos = parseInt($(currentDisplay.bgImage).css("z-index")); //Get z-index position of current background.
 
-    //Check if the new background's z-index is higher than the current BG. If so, their values must be swapped
+    //Check if the new background's z-index is higher than the current BG. If so, their values must be swapped.
     if (newBGPos > currentBGPos) {
         //Swap z-index values
         let temp = newBGPos;
         newBGPos = currentBGPos;
         currentBGPos = temp;
 
-        //Make the new background hidden behind the current background by switching their z-index positions.
+        //Make the new background below/behind the current background by switching their z-index positions.
         $(currentDisplay.bgImage).css("z-index", currentBGPos);  
         $(newDisplay.bgImage).css("z-index", newBGPos); //By having the lower z-index, its hidden behind the current background.
     }
 }
 
-
-//Light up the text that the user clicked on, and turn off the previous.
-function updateTabLighting(newDisplay, currentDisplay) {
-    $(newDisplay.tab).css("text-shadow", "2px 2px 8px white");
-    $(currentDisplay.tab).css("text-shadow", "none");
-}
-
 //Crossfade from the previous background/section into the one that the user selected.
 function crossfade(newDisplay, currentDisplay) {
     crossfadeBG(newDisplay, currentDisplay); //Crossfade from the current background to the new background.
-    crossfadeSect(newDisplay, currentDisplay); //Crossfade from the current section to the new section.
+    crossfadeSect(newDisplay, currentDisplay); //Crossfade from the current section text to the new section text.
 }
 
 //Crossfade from the current background to the new background.
 function crossfadeBG(newDisplay, currentDisplay) {
-    $(newDisplay.bgImage).animate({opacity: '1'}, 0, function() { //Make the new background have full opacity instantly while it's still behind the current background
+    //Note that a callback function is required to prevent a white flash in the background. First make the new display visible, and THEN fade the current background.
+    $(newDisplay.bgImage).animate({opacity: '1'}, 0, function() { //Make the new background have full opacity instantly while it's still below/behind the current (and visible) background.
             $(currentDisplay.bgImage).animate({opacity: '0'}, 600); //Fade the current background at the front in a span of 600 milliseconds, making it "crossfade" into the new background.
     }); 
 }
 
 //Crossfade from the current section to the new section.
 function crossfadeSect(newDisplay, currentDisplay) {
+    //Callback function not required. It seems to crossfade just fine like this.
     $(newDisplay.section).animate({opacity: '1'}, 600);
     $(currentDisplay.section).animate({opacity: '0'}, 600);
 }
