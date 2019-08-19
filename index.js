912287805da3c76566a4a06e8aada4f3dab64370
Brian Figueroa <brianfigueroa001@gmail.com>
2019-08-19 15:15:35 -0700

    Added icons and overlapped section elements

--- index.js
+++ index.js
@@ -16,5 +16,21 @@ $(document).ready(function() {
 	$("#projGolemPic").click(function() {
 		window.location.href= "\GolemTextConverter/GolemTextConverter.html";	
 	});
+	
+	$("#CalcPic").hover(function() {
+		$(this).animate({
+			width: '13.3em',
+			height: '13.3em',
+		}, 0.3);
+	}, function() {
+		$(this).animate({
+			width: '13em',
+			height: '13em',
+		}, 0.3);
+	});
+
+	$("#CalcPic").click(function() {
+		window.location.href= "\GolemTextConverter/GolemTextConverter.html";	
+	});	
 });
 
