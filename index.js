233e93f8e997e36d569d70aa5c778a856de3948b
Brian Figueroa <brianfigueroa001@gmail.com>
2019-08-20 14:58:43 -0700

    Added code to allow user to navigate sections

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 //Used to animate the background
 $(document).ready(function() {
-	$("#CalcPic").hover(function() {
-		$(this).animate({
-			width: '13.3em',
-			height: '13.3em',
-		}, 0.3);
+	//Store the current section being displayed;
+	let currentSection = "#homeSection";
+	
+	//Highlight whichever icon the user has their mouse over.
+	$("#homeIconEle, #projIconEle, #expIconEle, #eduIconEle").hover(function(){
+		$(this).css("color", "blue");
 	}, function() {
-		$(this).animate({
-			width: '13em',
-			height: '13em',
-		}, 0.3);
+		$(this).css("color", "white");
 	});
-
-	$("#projGolemPic").click(function() {
+	
+	//Change section information based on which navigation icon the user clicks on.
+	$("#homeIconEle, #projIconEle, #expIconEle, #eduIconEle").click(function(){
+		let selectedSection; //Stores the section that the user wants to display
+		
+		if ($("#homeIconEle").is(this)) {
+			selectedSection = "#homeSection";
+		}
+		else if ($("#projIconEle").is(this)) {
+			selectedSection = "#projSection";
+		}
+		else if ($("#expIconEle").is(this)) {
+			selectedSection = "#expSection";
+		}
+		else {
+			selectedSection = "#eduSection";
+		}
+		
+		//Prevents a bug by not running code if the selected section is already displayed.
+		if (selectedSection !== currentSection)
+		{
+			//Display the selected section and hide the current section.
+			$(selectedSection).css("display", "block");
+			$(currentSection).css("display", "none");
+			
+			//Update the current section
+			currentSection = selectedSection;
+		}
+	});
+	
+	//Links to projects when the image is clicked.
+	$("#projGolemPic").click(function(){
 		window.location.href= "\GolemTextConverter/GolemTextConverter.html";	
 	});
 
-	$("#CalcPic").click(function() {
+	$("#CalcPic").click(function(){
 		window.location.href= "\GolemTextConverter/GolemTextConverter.html";	
 	});	
 });
