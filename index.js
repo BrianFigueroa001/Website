75fdaa1bd519533e545cf1e22f90e09eb933a820
Brian <brianfigueroa001@gmail.com>
2020-08-29 21:23:01 -0400

    Updated website display format and text

--- index.js
+++ index.js
@@ -2,18 +2,38 @@
 $(document).ready(function() {
 	//Store the current section being displayed;
 	let currentSection = "#homeSection";
-	
+	let currentExpText = "#zgText";
+
+	$("#homeIconEle").css("color", "#0099ff");
+	$("#zgPic").css("box-shadow", "0vw 0vw 3vw blue");
+
 	//Highlight whichever icon the user has their mouse over.
-	$("#homeIconEle, #projIconEle, #expIconEle, #eduIconEle").hover(function(){
-		$(this).css("color", "blue");
-	}, function() {
-		$(this).css("color", "white");
-	});
-	
-	//Change section information based on which navigation icon the user clicks on.
 	$("#homeIconEle, #projIconEle, #expIconEle, #eduIconEle").click(function(){
+		$(this).css("color", "#0099ff");
+
+		if (!$(this).is( $("#homeIconEle")))
+		{
+			$("#homeIconEle").css("color", "white");
+		}
+
+		if (!$(this).is( $("#projIconEle")))
+		{
+			$("#projIconEle").css("color", "white");
+		}
+
+		if (!$(this).is( $("#expIconEle")))
+		{
+			$("#expIconEle").css("color", "white");
+		}
+
+		if(!$(this).is( $("#eduIconEle")))
+		{
+			$("#eduIconEle").css("color", "white");
+		}
+
+		//Change section information based on which navigation icon the user clicks on.
 		let selectedSection; //Stores the section that the user wants to display
-		
+
 		if ($("#homeIconEle").is(this)) {
 			selectedSection = "#homeSection";
 		}
@@ -26,26 +46,65 @@ $(document).ready(function() {
 		else {
 			selectedSection = "#eduSection";
 		}
-		
+
 		//Prevents a bug by not running code if the selected section is already displayed.
 		if (selectedSection !== currentSection)
 		{
 			//Display the selected section and hide the current section.
 			$(selectedSection).css("display", "block");
 			$(currentSection).css("display", "none");
-			
+
 			//Update the current section
 			currentSection = selectedSection;
 		}
 	});
 	
-	//Links to projects when the image is clicked.
-	$("#projGolemPic").click(function(){
-		window.location.href= "\GolemTextConverter/GolemTextConverter.html";	
+   //    box-shadow: 0vw 0vw 1vw grey;
+
+	//Highlight whichever image the user clicked on in the experience section
+	$("#zgPic, #codeExplorersPic, #fiuPic").click(function(){
+		$(this).css("box-shadow", "0vw 0vw 3vw blue");
+
+		if (!$(this).is( $("#zgPic")))
+		{
+			$("#zgPic").css("box-shadow", "0vw 0vw 0vw grey");
+		}
+
+		if (!$(this).is($("#codeExplorersPic")))
+		{
+			$("#codeExplorersPic").css("box-shadow", "0vw 0vw 0vw grey");
+		}
+
+		if (!$(this).is( $("#fiuPic")))
+		{
+			$("#fiuPic").css("box-shadow", "0vw 0vw 0vw grey");
+		}
+
+
+		//Change section information based on which navigation icon the user clicks on.
+		let selectedExpText; //Stores the section that the user wants to display
+
+		if ($("#zgPic").is(this)) {
+			selectedExpText = "#zgText";
+		}
+		else if ($("#codeExplorersPic").is(this)) {
+			selectedExpText = "#codeExplorersText";
+		}
+		else {
+			selectedExpText = "#fiuText";
+		}
+
+		//Prevents a bug by not running code if the selected section is already displayed.
+		if (selectedExpText !== currentExpText)
+		{
+			//Display the selected section and hide the current section.
+			$(selectedExpText).css("display", "block");
+			$(currentExpText).css("display", "none");
+
+			//Update the current section
+			currentExpText = selectedExpText;
+		}
 	});
 
-	$("#CalcPic").click(function(){
-		window.location.href= "\Calculator/Calculator.html";	
-	});	
 });
 
